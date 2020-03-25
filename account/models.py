de460dd30ff7a437d28168e4721a9103e2cac7eb
Д.И, <none@none>
2020-03-25 14:30:59 +0700

    Разбираюсь со слиянием веток 1

---
+++ account/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='images/', blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
---
+++ account/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+# from .views import Gallery
+
+urlpatterns = [
+    # path('', Gallery, name='gallery'),
+
+]
