28d6f3bfddddc0d296315343f82f4d56c1a3d804
Д.И, <none@none>
2020-04-02 15:34:47 +0700

    A profile model has been created that extends the user model
    --HG--
    branch : changing def views to class views

--- account/models.py
+++ account/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db.models import signals
+
+
+def create_user(sender, instance, created, **kwargs):
+    Profile.objects.create(user=instance)
 
 
 class Profile(models.Model):
     # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, blank=True, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
         return self.user.username
 
+    # class Meta:
+    #     verbose_name_plural = 'Профилей'
+    #     verbose_name = 'Профиль'
 
-class Events(models.Model):
-    pass
+
+signals.post_save.connect(create_user, sender=User)
